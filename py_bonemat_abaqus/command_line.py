c39a21b8ab10fc2b77f98f1f85ccf01d318c42c1
Elise <elisepegg@users.noreply.github.com>
2015-08-06 16:06:31 +0100

    Create command_line.py

---
+++ py_bonemat_abaqus/command_line.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+#
+# py_bonemat_abaqus - command line
+# ================================
+#
+# Version: 1.0.1
+# Created by Elise Pegg, University of Oxford, Aug 2015
+
+__all__ = ['main']
+
+#------------------------------------------------------------------------------
+# Import modules
+#------------------------------------------------------------------------------
+from py_bonemat_abaqus.run import run
+import sys, os
+import argparse
+
+#------------------------------------------------------------------------------
+# Run program
+#------------------------------------------------------------------------------
+def main():
+    parser = argparse.ArgumentParser(prog="py_bonemat_abaqus")
+    parser.add_argument('-p', nargs='?', help='parameters file (*.txt)')
+    parser.add_argument('-ct', nargs='?', help='ct dataset (dir or *.vtk)')
+    parser.add_argument('-m', nargs='?', help='mesh file (*.inp or *.ntr')
+ 
+    args = parser.parse_args()
+ 
+    run(args.p, args.ct, args.m)
+

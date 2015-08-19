9fd1d65d507e2bb40c1026b9f293d83b88fc8298
Elise <elisepegg@users.noreply.github.com>
2015-08-19 18:37:27 +0100

    Create version.py
    New file to enable automatic(ish) version control

---
+++ py_bonemat_abaqus/version.py
@@ -0,0 +1,35 @@
+__version__ = "1.0.2"
+__date__ = "Aug 2015"
+__author__ = "Elise Pegg, University of Oxford"
+__copyright__ = "Copyright 2015, University of Oxford, UK"
+__license__ = "GPLv3"
+__email__ = "elise.pegg@ndorms.ox.ac.uk"
+__status__ = "Development"
+__structure__ = """
+|--- gpl.txt
+|--- LICENSE.txt
+|--- MANIFEST.in
+|--- py_bonemat_abaqus
+|   |--- calc.py
+|   |--- classes.py
+|   |--- command_line.py
+|   |--- data_import.py
+|   |--- data_output.py
+|   |--- example
+|   |   |--- example_abaqus_mesh.inp
+|   |   |--- example_ct_data.vtk
+|   |   |--- example_parameters.txt
+|   |--- general.py
+|   |--- __init__.py
+|   |--- run.py
+|   |--- tests
+|   |   |--- __init__.py
+|   |   |--- tests_functional.py
+|   |   |--- tests.py
+|   |   |--- tests_validation.py
+|   |---version.py
+|--- py_bonemat_abaqus.py
+|--- README.rst
+|--- setup.cfg
+|--- setup.py
+"""
